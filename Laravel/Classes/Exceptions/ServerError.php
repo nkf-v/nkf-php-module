c0f7f74120ab500a34887e5bd181e98f1bb029e0
Vasiliy Nikiforov <kaktusv6@gmail.com>
2020-12-19 23:17:34 +1000

    Create server error for laravel projects

---
+++ Laravel/Classes/Exceptions/ServerError.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Nkf\Laravel\Classes\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Throwable;
+
+class ServerError extends Exception
+{
+    protected $errors;
+
+    public function __construct($errors, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->errors = !is_array($errors) ? [$errors] : $errors;
+    }
+
+    public function render(Request $request) : JsonResponse
+    {
+        $result = [];
+        foreach ($this->errors as $key => $errors)
+            foreach ($errors as $error)
+                $result[$key][] = [
+                    'code' => $error,
+//                'message' => '', // TODO get message by code from lang file
+                ];
+
+        return response()->json(['errors' => $result])->setStatusCode(500);
+    }
+}
