cd0861e4b9c3c40c0dc0dee7093cdc154686ce65
Vasiliy Nikiforov <kaktusv6@gmail.com>
2020-12-30 23:13:33 +1000

    Update BaseEnums

--- General/Classes/BaseEnums.php
+++ General/Classes/BaseEnums.php
@@ -4,10 +4,19 @@ namespace Nkf\General\Classes;
 
 use ReflectionClass;
 
-class BaseEnums
+abstract class BaseEnums
 {
     public static function getVariables() : array
     {
-        return (new ReflectionClass(static::class))->getConstants();
+        return array_change_key_case((new ReflectionClass(static::class))->getConstants());
     }
+
+    public static function getValues() : array
+    {
+        return array_flip(self::getVariables());
+    }
+
+    abstract static public function getLabels() : array;
+
+    abstract static public function getLabelValue(int $value);
 }
