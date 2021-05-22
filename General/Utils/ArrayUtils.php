a1e7862be799fe5e2bc8e28de6ef23d29c0f34e2
Vasiliy Nikiforov <kaktusv6@gmail.com>
2021-05-22 20:34:07 +1000

    ArrayUtils: Add methods for cast and get randmo many values

--- General/Utils/ArrayUtils.php
+++ General/Utils/ArrayUtils.php
@@ -2,10 +2,36 @@
 
 namespace Nkf\General\Utils;
 
-class ArrayUtils
+final class ArrayUtils
 {
     public static function randomValue(array $values)
     {
         return $values[array_rand($values)];
+    }
+
+    public static function randomValues(array $values, ?int $count = null) : array
+    {
+        if ($count === null)
+            $count = random_int(1, count($values));
+
+        $result = [];
+        do
+        {
+            $result[] = self::randomValue($values);
+        }
+        while (count($result) !== $count);
+        return $result;
+    }
+
+    public static function toArray(iterable $values)
+    {
+        if (is_array($values))
+            return $values;
+
+        $result = [];
+        foreach ($values as $value)
+            $result[] = $value;
+
+        return $result;
     }
 }
