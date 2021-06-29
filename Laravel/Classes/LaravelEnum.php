e33c73d4d3b304a042027d4f781c8ff935b73b53
Vasiliy Nikiforov <kaktusv6@gmail.com>
2021-06-29 20:09:47 +1000

    LravelEnum: Refactor code

--- Laravel/Classes/LaravelEnum.php
+++ Laravel/Classes/LaravelEnum.php
@@ -6,12 +6,13 @@ use Nkf\General\Classes\BaseEnum;
 
 class LaravelEnum extends BaseEnum
 {
+    // TODO Перенести в другой класс, enum не должен тметь эти функции поскольку это не его задачи
     static public function getLabels() : array
     {
         return trans(sprintf('enums.%s', static::class));
     }
 
-    static public function getLabelValue(int $value)
+    static public function getLabelValue(int $value) : string
     {
         return trans(sprintf('enums.%s.%d', static::class, $value));
     }
