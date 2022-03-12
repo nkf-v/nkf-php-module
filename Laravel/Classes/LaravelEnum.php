778a92402acfb0f5de011c293e63acef1d32726a
Vasiliy Nikiforov <kaktusv6@gmail.com>
2022-03-12 12:56:41 +1000

    Refactor todo to BaseEnum

--- Laravel/Classes/LaravelEnum.php
+++ Laravel/Classes/LaravelEnum.php
@@ -6,7 +6,7 @@ use Nkf\General\Classes\BaseEnum;
 
 class LaravelEnum extends BaseEnum
 {
-    // TODO Перенести в другой класс, enum не должен тметь эти функции поскольку это не его задачи
+    // TODO Перенести в другой класс, enum не должен иметь эти функции поскольку это не его задачи
     static public function getLabels() : array
     {
         return trans(sprintf('enums.%s', static::class));
